0ef034f4234f9a9b7dc656b1798716f85cbdc53c
Omar Gonzalez <ogonzaleznyc@gmail.com>
2019-11-10 08:52:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,12 +19,11 @@
 //   }
 // }
 
-class BoardMember {
-  constructor(name, homeState, training) {
+class BoardMember(name, homeState, training) {
     this.name = name;
     this.homeState = homeState;
     this.training = training;
-  }
+  
   
   veto() {
     return "No, I must disagree"
