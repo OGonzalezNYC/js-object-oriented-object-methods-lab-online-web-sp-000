ff028895158fe5ee1f45e69241c081607d1624c8
Omar Gonzalez <ogonzaleznyc@gmail.com>
2019-11-10 08:49:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,48 +1,20 @@
-// function BoardMember (name, homeState, training) {
-//   this.name = name;
-//   this.homeState = homeState;
-//   this.training = training;
-//   this.veto = function() {
-//     return "No, I must disagree"
-//   }
-//   this.approve = function() {
-//     return "You can do that!"
-//   }
-//   this.doCharity = function() {
-//     return "I like to help people."
-//   }
-//   this.releasePressStatement = function() {
-//     return "You will see great things from Scuber."
-//   }
-//   this.sayHi = function() {
-//     return `Hi, my name is ${this.name}. I am from ${homeState}, and I was trained in ${training}.`
-//   }
-// }
-
-class BoardMember {
-  constructor(name, homeState, training) {
-    this.name = name;
-    this.homeState = homeState;
-    this.training = training;
-  }
-  
-  veto() {
+function BoardMember (name, homeState, training) {
+  this.name = name;
+  this.homeState = homeState;
+  this.training = training;
+  this.veto = function() {
     return "No, I must disagree"
   }
-  
-  approve() {
+  this.approve = function() {
     return "You can do that!"
   }
-  
-  doCharity() {
+  this.doCharity = function() {
     return "I like to help people."
   }
-  
-  releasePressStatement() {
+  this.releasePressStatement = function() {
     return "You will see great things from Scuber."
   }
-  
-  sayHi() {
-    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+  this.sayHi = function() {
+    return `Hi, my name is ${this.name}. I am from ${homeState}, and I was trained in ${training}.`
   }
 }
